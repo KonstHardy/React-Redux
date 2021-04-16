a29e57a16ea9cb6858d557695a00ba07127f6dfa
KonstHardy <konst.hardy@gmail.com>
2021-04-16 17:02:16 +0300

    [fix] styles
    [fix] styles;

--- src/components/addComment/index.js
+++ src/components/addComment/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { mapStateToProps, mapDispatchToProps } from "../../store/dispatch";
-import logo from "./../../img/logo192.png";
 import "./addComment.css";
 
 class AddComment extends Component {
@@ -59,12 +58,12 @@ class AddComment extends Component {
             <div className="form__comment">
               <input
                 className="form__inputName"
-                required
-                placeholder="Enter your name"
                 type="text"
                 name="inputName"
                 value={this.state.inputName}
                 onChange={this.handleChange}
+                placeholder="Enter your name"
+                required
               ></input>
               <button className="btn__submit" type="submit">
                 Add comment
@@ -72,15 +71,15 @@ class AddComment extends Component {
             </div>
             <textarea
               className="form__inputText"
-              required
-              placeholder="Enter your comment"
               type="text"
               name="inputText"
               value={this.state.inputText}
               onChange={this.handleChange}
+              placeholder="Enter your comment"
+              required
+              rows="1"
             ></textarea>
           </form>
-          <img className="img__react" src={logo} alt="React logo" />
         </div>
       </>
     );
--- src/containers/app.js
+++ src/containers/app.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { mapStateToProps, mapDispatchToProps } from "../store/dispatch";
 
+import logo from "./../img/logo512.png";
 import AddComment from "../components/addComment";
 import ListComments from "../components/listComments";
 import "./app.css";
@@ -12,6 +13,7 @@ let App = (props) => {
   return (
     <div className="wrapper">
       <h1 className="title">react-redux__comments-app</h1>
+      <img className="img__react" src={logo} alt="React logo" />
       <AddComment addComment={addComment} />
       <ListComments comments={comments} removeComment={removeComment} />
     </div>
